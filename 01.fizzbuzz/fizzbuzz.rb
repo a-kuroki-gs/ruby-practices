6bcc50a89c6d0ac0660482ea4692f63e01adf1f9
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-08-09 09:26:27 +0900

    n += 1 を削除し訂正しました

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,14 @@
 def fizzbuzz(first, last)
   (first..last).each do |n|
-  if n % 15 == 0
-    puts "FizzBuzz"
-  elsif n % 3 == 0
-    puts "Fizz"
-  elsif n % 5 == 0
-    puts "Buzz"
-  else
-    puts "#{n}"
-  end
-    n += 1
+    if n % 15 == 0
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else
+      puts "#{n}"
+    end
   end
 end
 
