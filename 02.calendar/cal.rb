7ebe4331241ea3024b88a21b2e8c96543ec896a1
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-08-16 16:22:28 +0900

    cal.rbの訂正をいくつか行い、第２回分を提出しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,82 +1,47 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
 opt = OptionParser.new
 
 option = {}
-opt.on('-y') {|v| v}
-opt.on('-m') {|v| v}
+opt.on('-y VAL') { |v| option[:y] = v }
+opt.on('-m VAL') { |v| option[:m] = v }
 
-opt.parse!(ARGV, into: option)
+opt.parse!(ARGV)
 
-if option[:y] == true && option[:m] == true
-    @y = ARGV[0]
-    @m = ARGV[1]
-  elsif option[:y] == true
-    @y = ARGV[0]
-    @m = Date.today.month
-  elsif option[:m] == true
-    @y = Date.today.year
-    @m = ARGV[0]
-  else
-    @y = Date.today.year
-    @m = Date.today.month
+y = option[:y].nil? ? Date.today.year.to_i : option[:y].to_i
+m = option[:m].nil? ? Date.today.month.to_i : option[:m].to_i
+
+def print_day(date)
+  print date == Date.today ? "\e[30m\e[47m#{date.day.to_s.rjust(2)}\e[0m " : date.day.to_s.rjust(2) + ' '
+  puts if date.saturday?
 end
 
-def print_d(d)
-  if d.saturday?
-    puts "#{d.strftime('%e')}"
-  else
-    print "#{d.strftime('%e')} "
-  end
-end
-
-def print_today(d)
-  if d.saturday?
-    puts "\e[30m\e[47m#{d.strftime('%e')}\e[0m "
-  else
-    print "\e[30m\e[47m#{d.strftime('%e')}\e[0m "
-  end
-end
-
-def p_calender(year, month)
+def print_calender(year, month)
   puts "      #{month}月 #{year}"
-  puts "日 月 火 水 木 金 土"
-  
+  puts '日 月 火 水 木 金 土'
+
   first = Date.new(year, month, 1)
   last = Date.new(year, month, -1)
   days = (first..last)
-  
-  space_first = first.strftime('%w').to_i
-  space_first.times do
-    print "   "
-  end
-  
+
+  offset = first.wday
+  print '   ' * offset
+
   days.each do |day|
-    if day == Date.today
-      print_today(day)
-    else
-      print_d(day)
-    end
+    print_day(day)
   end
   puts
-  
 end
 
-@y = @y.to_i
-@m = @m.to_i
-
-if @y >= 1
-  if @m >= 1 && @m <= 12
-    p_calender(@y, @m)
+if y >= 1
+  if m >= 1 && m <= 12
+    print_calender(y, m)
   else
-    puts "cal: #{@m} is neither a month number (1..12) nor a name"
+    puts "cal: #{m} is neither a month number (1..12) nor a name"
   end
 else
-  if @y == 0
-    puts "cal: year `0' not in range 1..9999"
-  else
-    "その他のエラー"
-  end
+  puts '不正な入力です'
 end
