85b0d2e1135a8461347ddff934eb6a1b4b243cf7
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-10-10 16:40:01 +0900

    メソッド名、変数名を変更しました
    メソッド名につけるべきでないgetをつけていたため変更
    計算するメソッドにはcalculate、探すメソッドにはfindをつける
    また、infoではわかりにくいためinfoがついているものを変更
    メソッド名はdata_and_max_length・sum_and_max_lengthに変更
    変数名はdata_count・配列名はdata_countsに変更

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -7,24 +7,24 @@ def calculate_max_length(count_number, max_length)
   [max_length, count_number.to_s.length].max
 end
 
-def get_file_info(file, max_lengths_of_data_size)
-  file_info = { lines: 0, words: 0, bytes: 0, path: nil }
+def find_file_data_and_max_lengths(file, max_lengths_of_data_size)
+  data_count = { lines: 0, words: 0, bytes: 0, path: nil }
 
   file.each_line do |l|
-    file_info[:lines] += 1
+    data_count[:lines] += 1
     word_array = l.split(' ')
-    file_info[:words] += word_array.count
+    data_count[:words] += word_array.count
   end
-  file_info[:bytes] = file.bytesize
+  data_count[:bytes] = file.bytesize
 
-  max_lengths_of_data_size[:lines] = calculate_max_length(file_info[:lines], max_lengths_of_data_size[:lines])
-  max_lengths_of_data_size[:words] = calculate_max_length(file_info[:words], max_lengths_of_data_size[:words])
-  max_lengths_of_data_size[:bytes] = calculate_max_length(file_info[:bytes], max_lengths_of_data_size[:bytes])
+  max_lengths_of_data_size[:lines] = calculate_max_length(data_count[:lines], max_lengths_of_data_size[:lines])
+  max_lengths_of_data_size[:words] = calculate_max_length(data_count[:words], max_lengths_of_data_size[:words])
+  max_lengths_of_data_size[:bytes] = calculate_max_length(data_count[:bytes], max_lengths_of_data_size[:bytes])
 
-  [file_info, max_lengths_of_data_size]
+  [data_count, max_lengths_of_data_size]
 end
 
-def get_max_length_of_stdin(params, max_lengths_of_data_size)
+def calculate_max_length_of_stdin(params, max_lengths_of_data_size)
   if params.values.count(true) == 1
     0
   elsif max_lengths_of_data_size.values.max < 8
@@ -34,7 +34,7 @@ def get_max_length_of_stdin(params, max_lengths_of_data_size)
   end
 end
 
-def get_max_length_of_file(params, max_lengths_of_data_size)
+def calculate_max_length_of_file(params, max_lengths_of_data_size)
   if params.values.count(true) == 1
     0
   else
@@ -42,19 +42,19 @@ def get_max_length_of_file(params, max_lengths_of_data_size)
   end
 end
 
-def get_sum_info(file_infos)
-  file_info_sum = { lines: 0, words: 0, bytes: 0, path: '合計' }
+def calculate_sum_and_max_length(data_counts)
+  data_count_sum = { lines: 0, words: 0, bytes: 0, path: '合計' }
   max_length_of_sum = 0
-  file_infos.each do |info|
-    file_info_sum[:lines] += info[:lines]
-    file_info_sum[:words] += info[:words]
-    file_info_sum[:bytes] += info[:bytes]
+  data_counts.each do |data_count|
+    data_count_sum[:lines] += data_count[:lines]
+    data_count_sum[:words] += data_count[:words]
+    data_count_sum[:bytes] += data_count[:bytes]
 
-    max_length_of_sum = [max_length_of_sum, [file_info_sum[:lines], file_info_sum[:words], file_info_sum[:bytes]].max.to_s.length].max
+    max_length_of_sum = [max_length_of_sum, [data_count_sum[:lines], data_count_sum[:words], data_count_sum[:bytes]].max.to_s.length].max
   end
-  file_infos << file_info_sum
+  data_counts << data_count_sum
 
-  [file_infos, max_length_of_sum]
+  [data_counts, max_length_of_sum]
 end
 
 opt = OptionParser.new
@@ -65,42 +65,42 @@ opt.on('-w') { |v| params[:w] = v }
 opt.on('-c') { |v| params[:c] = v }
 opt.parse!(ARGV)
 
-file_infos = []
+data_counts = []
 string_lengths_of_data_size = { lines: 0, words: 0, bytes: 0, path: 0 }
 
 if ARGV.empty?
-  file_info, string_lengths_of_data_size = get_file_info($stdin.read, string_lengths_of_data_size)
-  file_infos << file_info
+  data_count, string_lengths_of_data_size = find_file_data_and_max_lengths($stdin.read, string_lengths_of_data_size)
+  data_counts << data_count
 
-  string_length = get_max_length_of_stdin(params, string_lengths_of_data_size)
+  string_length = calculate_max_length_of_stdin(params, string_lengths_of_data_size)
 
 else
   ARGV.each do |file_path|
-    file_info, string_lengths_of_data_size = get_file_info(File.read(file_path), string_lengths_of_data_size)
-    file_info[:path] = file_path
-    file_infos << file_info
+    data_count, string_lengths_of_data_size = find_file_data_and_max_lengths(File.read(file_path), string_lengths_of_data_size)
+    data_count[:path] = file_path
+    data_counts << data_count
   end
 
   if ARGV.count == 1
-    string_length = get_max_length_of_file(params, string_lengths_of_data_size)
+    string_length = calculate_max_length_of_file(params, string_lengths_of_data_size)
   else
-    file_infos, string_length_of_sum = get_sum_info(file_infos)
+    data_counts, string_length_of_sum = calculate_sum_and_max_length(data_counts)
     string_length = [string_lengths_of_data_size.values.max, string_length_of_sum].max
   end
 end
 
-file_infos.each do |info|
+data_counts.each do |data_count|
   if params[:l] || params.none?
-    print info[:lines].to_s.rjust(string_length)
+    print data_count[:lines].to_s.rjust(string_length)
     print ' '
   end
   if params[:w] || params.none?
-    print info[:words].to_s.rjust(string_length)
+    print data_count[:words].to_s.rjust(string_length)
     print ' '
   end
   if params[:c] || params.none?
-    print info[:bytes].to_s.rjust(string_length)
+    print data_count[:bytes].to_s.rjust(string_length)
     print ' '
   end
-  puts info[:path]
+  puts data_count[:path]
 end
