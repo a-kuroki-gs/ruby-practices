a31954a5501b70f15a9b0550cdd888e293a8ab5c
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-11-27 14:55:54 +0900

    外部からアクセスする必要のないメソッドをprivateメソッドにする

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -17,6 +17,22 @@ class Game
     @frames << Frame.new(game_marks[0], game_marks[1], game_marks[2])
   end
 
+  def score
+    game_score = @frames.map(&:score).sum
+
+    @frames.each_cons(3) do |current_frame, next_frame, after_next_frame|
+      if current_frame.strike?
+        game_score += bonus_of_strike(next_frame, after_next_frame)
+      elsif current_frame.spare?
+        game_score += bonus_of_spare(next_frame)
+      end
+    end
+
+    game_score += bonus_of_ninth_frame(@frames[-2], @frames[-1]).to_i
+  end
+
+  private
+
   def bonus_of_strike(next_frame, after_next_frame)
     if next_frame.strike?
       10 + after_next_frame.first_shot.score
@@ -35,19 +51,5 @@ class Game
     elsif ninth_frame.spare?
       last_frame.first_shot.score
     end
-  end
-
-  def score
-    game_score = @frames.map(&:score).sum
-
-    @frames.each_cons(3) do |current_frame, next_frame, after_next_frame|
-      if current_frame.strike?
-        game_score += bonus_of_strike(next_frame, after_next_frame)
-      elsif current_frame.spare?
-        game_score += bonus_of_spare(next_frame)
-      end
-    end
-
-    game_score += bonus_of_ninth_frame(@frames[-2], @frames[-1]).to_i
   end
 end
