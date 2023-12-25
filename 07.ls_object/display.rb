943df2ecbf8b416b9368a9e2f8e61cbcdf180526
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-25 12:11:12 +0900

    変数名を修正

--- 07.ls_object/display.rb
+++ 07.ls_object/display.rb
@@ -30,21 +30,21 @@ class Display
   end
 
   def print_detailed_list
-    max_nlink = @file_stats.map { |file| file.nlink.to_s.size }.max
-    max_user = @file_stats.map { |file| file.user.size }.max
-    max_group = @file_stats.map { |file| file.group.size }.max
-    max_size = @file_stats.map { |file| file.bytesize.to_s.size }.max
+    max_nlink = @file_stats.map { |file_stat| file_stat.nlink.to_s.size }.max
+    max_user = @file_stats.map { |file_stat| file_stat.user.size }.max
+    max_group = @file_stats.map { |file_stat| file_stat.group.size }.max
+    max_size = @file_stats.map { |file_stat| file_stat.bytesize.to_s.size }.max
 
     puts "合計 #{@file_stats.sum(&:blocks) / 2}"
-    @file_stats.each do |file|
+    @file_stats.each do |file_stat|
       puts [
-        file.mode,
-        file.nlink.to_s.rjust(max_nlink),
-        file.user.ljust(max_user),
-        file.group.ljust(max_group),
-        file.bytesize.to_s.rjust(max_size),
-        format('%<month>3s%<day_and_time>9s', month: file.mtime.strftime('%-m月'), day_and_time: file.mtime.strftime('%e %H:%M')),
-        file.name
+        file_stat.mode,
+        file_stat.nlink.to_s.rjust(max_nlink),
+        file_stat.user.ljust(max_user),
+        file_stat.group.ljust(max_group),
+        file_stat.bytesize.to_s.rjust(max_size),
+        format('%<month>3s%<day_and_time>9s', month: file_stat.mtime.strftime('%-m月'), day_and_time: file_stat.mtime.strftime('%e %H:%M')),
+        file_stat.name
       ].join(' ')
     end
   end
